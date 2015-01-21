c6f7b5f4c8da0070cfdff6fab7a522e3d0661bda
Zachary Toth <zachtoth@gmail.com>
2015-01-21 13:21:03 -0500

    Exercise6 part 3

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,19 @@
+#1
 $grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+#method for part 2
 def grocery_output
 	$grocery_list.each { |item| puts "*#{item}" }
 end
 
 grocery_output
 
+#2
 $grocery_list << "rice"
 
 grocery_output
+
+#3
+puts $grocery_list.count 
+
+#4
